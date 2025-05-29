8ac90b6dcdee2bd28dbbeb2db78c9853508a528e
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-05-29 13:33:56 +0800

    feat: implement InputComponent with inputs and lifecycle methods for enhanced functionality

--- src/app/shared/components/ui/input/input.component.ts
+++ src/app/shared/components/ui/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, type AfterContentInit } from "@angular/core";
 
 @Component({
     selector: "app-input",
@@ -6,4 +6,33 @@ import { Component } from "@angular/core";
     templateUrl: "./input.component.html",
     styleUrl: "./input.component.css",
 })
-export class InputComponent {}
+export class InputComponent implements AfterContentInit {
+    @Input() type: "text" | "email" | "password" | "number" | "tel" = "text";
+    @Input() placeholder: string | null = null;
+    @Input() value: string | null = null;
+    @Input() disabled = false;
+    @Input() required = false;
+    @Input() readonly = false;
+    @Input() size: "xs" | "sm" | "md" | "lg" = "md";
+    @Input() error: string | null = null;
+
+    inputType: "text" | "email" | "password" | "number" | "tel" = "text";
+    inputPlaceholder: string | null = null;
+    inputValue: string | null = null;
+    inputDisabled = false;
+    inputRequired = false;
+    inputReadonly = false;
+    inputSize: "xs" | "sm" | "md" | "lg" = "md";
+    inputError: string | null = null;
+
+    ngAfterContentInit(): void {
+        this.inputType = this.type;
+        this.inputPlaceholder = this.placeholder;
+        this.inputValue = this.value;
+        this.inputDisabled = this.disabled;
+        this.inputRequired = this.required;
+        this.inputReadonly = this.readonly;
+        this.inputSize = this.size;
+        this.inputError = this.error;
+    }
+}
